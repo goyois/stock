a39b9084d1df63abef93a9a55e54b7ffafcb1ec3
Jwwone <bau12288@gmail.com>
2026-06-19 22:02:50 +0900

    test: 동시 100개 요청 테스트

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -2,12 +2,17 @@ package com.example.stock.service;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repo.StockRepository;
+import org.apache.catalina.Executor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,5 +44,30 @@ class StockServiceTest {
         //100 - 1 = 99
         Stock stock = stockRepository.findById(1L).orElseThrow();
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_1000개의_요청() throws InterruptedException {
+        int threadCnt = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        //100 개의 요청이 끝날 때까지 기다려야하므로 다른 스레드에서 수행중인 작업이 끝날 떄까지 대기시켜줌
+        CountDownLatch latch = new CountDownLatch(threadCnt);
+
+        for (int i = 0; i < threadCnt; i++) {
+            executorService.submit(() -> {
+                try{
+                    stockService.decrease(1L,1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 - (1 * 100) = 0;
+        assertEquals(0, stock.getQuantity());
     }
 }
